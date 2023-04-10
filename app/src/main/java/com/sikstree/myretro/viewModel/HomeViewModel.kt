6c66bbe9c9a17d2fd5484f5355859e031da476a1
slondy <slondy@naver.com>
2023-04-10 22:07:22 +0900

    파이어베이스 연동 - 230410

--- app/src/main/java/com/sikstree/myretro/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/sikstree/myretro/viewModel/HomeViewModel.kt
@@ -2,8 +2,19 @@ package com.sikstree.myretro.viewModel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 
 class HomeViewModel( application: Application) : AndroidViewModel(application) {
+    init{
 
+    }
+
+
+    fun makeData() {
+        val database = FirebaseFirestore.getInstance()
+        database.collection("")
+    }
 }
