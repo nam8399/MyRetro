7a60ec334eb7b7ce1cd3e1baf3ca8f81c27a83b0
slondy <slondy@naver.com>
2023-05-15 13:26:46 +0900

    마이레트로 아이템 화면 커스텀 다이얼로그 구현 및 결제 기능 구현 - 230515

--- app/src/main/java/com/sikstree/myretro/View/Fragment/RetroShopFragment.kt
+++ app/src/main/java/com/sikstree/myretro/View/Fragment/RetroShopFragment.kt
@@ -1,11 +1,14 @@
 package com.sikstree.myretro.View.Fragment
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -62,11 +65,33 @@ class RetroShopFragment : Fragment() {
 
         viewModel.itemOnclickEvent.observe(viewLifecycleOwner, Observer {
             if (!"".equals(it)) {
-                intent.putExtra("url",viewModel.itemOnclickEvent.value)
-                startActivity(intent)
+                customDialogInit() // 결제 다이얼로그 생성
+//                intent.putExtra("url",viewModel.itemOnclickEvent.value)
+//                startActivity(intent)
             }
         })
 
     }
+
+    private fun customDialogInit() { // 커스텀 다이얼로그 호출
+        val mDialogView = LayoutInflater.from(context).inflate(R.layout.custom_dialog, null)
+        val mBuilder = AlertDialog.Builder(context)
+            .setView(mDialogView)
+            .setTitle("타이틀")
+
+        val imgBtn = mDialogView.findViewById<ImageView>(R.id.itemImg)
+        val pay1Btn = mDialogView.findViewById<Button>(R.id.pay1)
+        val pay2Btn = mDialogView.findViewById<Button>(R.id.pay2)
+
+        pay1Btn.setOnClickListener() {
+            fragmentManager?.let { it1 -> viewModel.goRequest(it1, "나이스페이", 1000.0) }
+        }
+
+        mBuilder.show()
+
+    }
+
+
+
 
 }
--- app/src/main/java/com/sikstree/myretro/viewModel/RetroshopViewModel.kt
+++ app/src/main/java/com/sikstree/myretro/viewModel/RetroshopViewModel.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +22,12 @@ import com.google.firebase.ktx.Firebase
 import com.sikstree.myretro.Data.ItemData
 import com.sikstree.myretro.R
 import com.sikstree.myretro.View.Activity.MainActivity
+import kr.co.bootpay.android.Bootpay
+import kr.co.bootpay.android.events.BootpayEventListener
+import kr.co.bootpay.android.models.BootExtra
+import kr.co.bootpay.android.models.BootItem
+import kr.co.bootpay.android.models.BootUser
+import kr.co.bootpay.android.models.Payload
 
 
 class RetroshopViewModel(application: Application) : AndroidViewModel(application) {
@@ -49,7 +56,7 @@ class RetroshopViewModel(application: Application) : AndroidViewModel(applicatio
             firestore = FirebaseFirestore.getInstance()
             firestore?.collection("retroItems")?.orderBy("title", Query.Direction.DESCENDING)
                 ?.addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                    datas.clear()
+//                    datas.clear()
                     if (querySnapshot == null) return@addSnapshotListener
 
                     // 데이터 받아오기
@@ -106,5 +113,69 @@ class RetroshopViewModel(application: Application) : AndroidViewModel(applicatio
             return datas.size
         }
 
+    }
+
+
+
+    fun goRequest(fragmentManager: FragmentManager, pgName : String, price : Double) {
+        val user = BootUser().setPhone("010-2666-6951") // 구매자 정보
+        val extra = BootExtra()
+            .setCardQuota("0,2,3") // 일시불, 2개월, 3개월 할부 허용, 할부는 최대 12개월까지 사용됨 (5만원 이상 구매시 할부허용 범위)
+
+        val method = "카드"
+
+        val items: MutableList<BootItem> = ArrayList()
+        val item1 = BootItem().setName("마우스").setId("ITEM_CODE_MOUSE").setQty(1).setPrice(500.0)
+        val item2 = BootItem().setName("키보드").setId("ITEM_KEYBOARD_MOUSE").setQty(1).setPrice(500.0)
+        items.add(item1)
+        items.add(item2)
+
+        val payload = Payload()
+        payload.setApplicationId("6461aa773049c8001b96868b")
+            .setOrderName("부트페이 결제테스트")
+            .setPg(pgName)
+            .setOrderId("1234")
+            .setMethod(method)
+            .setPrice(price)
+            .setUser(user)
+            .setExtra(extra).items = items
+
+        val map: MutableMap<String, Any> = HashMap()
+        map["1"] = "abcdef"
+        map["2"] = "abcdef55"
+        map["3"] = 1234
+        payload.metadata = map
+
+        Bootpay.init(fragmentManager, getApplication())
+            .setPayload(payload)
+            .setEventListener(object : BootpayEventListener {
+                override fun onCancel(data: String) {
+                    Log.d("bootpay", "cancel: $data")
+                }
+
+                override fun onError(data: String) {
+                    Log.d("bootpay", "error: $data")
+                }
+
+                override fun onClose(data: String) {
+                    Log.d("bootpay", "close: $data")
+                    Bootpay.removePaymentWindow()
+                }
+
+                override fun onIssued(data: String) {
+                    Log.d("bootpay", "issued: $data")
+                }
+
+                override fun onConfirm(data: String): Boolean {
+                    Log.d("bootpay", "confirm: $data")
+                    //                        Bootpay.transactionConfirm(data); //재고가 있어서 결제를 진행하려 할때 true (방법 1)
+                    return true //재고가 있어서 결제를 진행하려 할때 true (방법 2)
+                    //                        return false; //결제를 진행하지 않을때 false
+                }
+
+                override fun onDone(data: String) {
+                    Log.d("done", data)
+                }
+            }).requestPayment()
     }
 }
